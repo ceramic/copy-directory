68c688d6f796e43f53bb691474c61ce2f9621e9e
Fernando Borretti <eudoxiahp@gmail.com>
2016-03-25 11:35:09 -0300

    Add tests

--- t/copy-directory.lisp
+++ t/copy-directory.lisp
@@ -17,6 +17,15 @@
     (is-true (probe-file dest))
     (is-true
      (probe-file (merge-pathnames #p"a.txt" dest)))
+    (uiop:delete-directory-tree dest :validate t))
+  (let ((source (asdf:system-relative-pathname :copy-directory #p"t/dir/"))
+        (dest (asdf:system-relative-pathname :copy-directory #p"t/deep/copy/")))
+    (is-false (probe-file dest))
+    (is
+     (pathnamep (copy-directory:copy source dest)))
+    (is-true (probe-file dest))
+    (is-true
+     (probe-file (merge-pathnames #p"a.txt" dest)))
     (uiop:delete-directory-tree dest :validate t)))
 
 (defun run-tests ()
